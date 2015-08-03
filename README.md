c00aa1537b5657f13be1dd975709a92027b9869f
Siegfried Puchbauer <spuchbauer@splunk.com>
2015-08-03 12:30:54 -0700

    Initial import

---
+++ README.md
@@ -0,0 +1,32 @@
+# Parallel Piper
+
+Simple mobile web app for sending data to a Splunk HTTP input. It measures the device motion and sends an event with 
+the intensity of the motion to Splunk.
+
+## Build/Deploy
+
+1) Install NPM deps
+```
+npm install
+```
+
+2) Run webpack build
+
+The following environment variables need to be defined for specifying the Splunk 
+server running the HTTP input. 
+
+- `PP_SPLUNK_HOST` (default is `"localhost"`)
+- `PP_SPLUNK_PORT` (default is `8088`)
+- `PP_SPLUNK_SSL` (default is `false`)
+- `PP_SPLUNK_TOKEN` (required) token for the HTTP input
+
+Then run the webpack build:
+
+```
+PP_SPLUNK_TOKEN=cafecafecafecafe \
+PP_SPLUNK_HOST=my.host.com \
+./node_modules/webpack/bin/webpack.js -p
+```
+
+This generates all the page, JS and all the static content in the `dist` directory. Simply
+copy it to a webserver-exposed folder.
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require("path");
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var DefinePlugin = require('webpack/lib/DefinePlugin');
+
+function fail(msg) {
+    throw new Error(msg);
+}
+
+module.exports = {
+    entry: 'app.js',
+    resolve: {
+        root: path.join(__dirname, 'lib'),
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'piper.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader?optional=runtime,jsxPragma=vdom"},
+            {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+            {test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")},
+            {test: /\.(woff|eot|ttf|png|svg)$/, loader: "file-loader"}
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("piper.css"),
+        new HtmlWebpackPlugin({
+            title: 'Parallel Piper',
+            template: 'index.html',
+            inject: 'body',
+            minify: {},
+            hash: true
+        }),
+        new DefinePlugin({
+            SPLUNK_HOST: JSON.stringify(process.env.PP_SPLUNK_HOST || 'localhost'),
+            SPLUNK_PORT: JSON.stringify(parseInt(process.env.PP_SPLUNK_PORT || 8088, 10)),
+            SPLUNK_SSL: JSON.stringify(process.env.PP_SPLUNK_SSL == 'true' || false),
+            SPLUNK_TOKEN: JSON.stringify(process.env.PP_SPLUNK_TOKEN || fail('Environment variable PP_SPLUNK_TOKEN is required.'))
+        })
+    ]
+};
